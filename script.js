eaa1bbf2a5ac7f611c558c6d1b6de36a1b8e0f26
jjj <gentildonna8282@gmail.com>
2026-07-19 03:34:56 +0900

    変更１

--- script.js
+++ script.js
@@ -11,9 +11,48 @@ document.getElementById("password-btn").addEventListener("click",function(){
         //パスワード画面を非表示にする//
         document.getElementById("password-screen").style.display = "none";
         //メイン画面を表示//
-        document.getElementById("main-screen").style.display = "block";
+        document.getElementById("quiz-screen").style.display = "block";
+        //最初の問題を表示
+        showQuestion();
     }else{
         //エラーメッセージを表示//
         document.getElementById("password-error").style.display = "block";
     }
 });
+
+//-----問題データ（画像パスだけ入れる）-----//
+const questions = [
+    {
+        questionImg: "images_graph/問題1.png",
+        choices: [
+            "images_graph/折れ線ボタン.png",
+            "images_graph/棒ボタン.png",
+            "images_graph/円ボタン.png",
+            "images_graph/ヒストグラムボタン.png"
+        ]
+    },
+];
+
+//-----ランダムに10問選ぶ-----//
+const shuffleQuestion = questions.sort(()=>Math.random()-0.5).slice(0,10);
+//現在の問題番号
+let currentIndex = 0;
+
+//-----第何問かを表示しつつ問題をセット-----//
+function showQuestion(){
+    //第○問の表示（1から数える）
+    document.getElementById("question-count").textContent =
+        `第 ${currentIndex + 1}問`;
+
+    //問題画像の表示
+    document.getElementById("question-image").src =
+        shuffleQuestion[currentIndex].questionImg;
+    
+    //選択肢画像の表示
+    const choiceButtons = 
+        document.querySelectorAll(".choice-btn img");
+    shuffleQuestion[currentIndex].choices.forEach((imgpath,i) => {
+        choiceButtons[i].src = imgpath;
+    });
+
+}
